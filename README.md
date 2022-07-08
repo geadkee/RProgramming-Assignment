c59298f9fd4c694b1b90f5a9164243ede76f68f9
Gead Kee <gangeadkee@gmail.com>
2022-07-08 14:15:13 +0800

    installed packages used and load them.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RProgramming-Assignment
 Assignment of module degree yr2 - "Programming for Data Analysis".
+Directly from Rstudio
---
+++ Rscript-for-PFDA-assignment.R
@@ -0,0 +1,19 @@
+## GAN GEAD KEE
+## TP057261
+
+# install.packages("viridis")
+# install.packages("hrbrthemes")
+# install.packages("gridExtra")
+# install.packages("plotly")
+# install.packages("GGally")
+
+library(viridis)  # colour scales
+library(hrbrthemes)
+library(gridExtra)
+library(crayon)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(GGally)
+
+
